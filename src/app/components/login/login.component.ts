80358018f2a6d73a2bad2b1398429b357cfe62cc
Anand Prakash Chaudhary <apchaudhary8126@gmail.com>
2024-04-11 10:07:40 +0530

    first commit

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,13 +1,41 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
 
+  userLogin!:FormGroup;
+
+  constructor(private fb:FormBuilder, private router:Router){}
+
+  ngOnInit(){
+    this.userloginForm();
+  }
+
+  userloginForm(){
+    this.userLogin=this.fb.group({
+      userid:[''],
+      password:[''],
+    })
+  }
+
+
+  onLoginUser(){
+    console.log(this.userLogin.value);
+    this.router.navigate(['/booking-tickets'])
+  }
+
+  showPassword() {
+    const inputPassword = document.getElementById(
+      'password'
+    ) as HTMLInputElement;
+    inputPassword.type = inputPassword.type === 'password' ? 'text' : 'password';
+  }
 }
